a60b8bbf33e9b1a852d0fae7b501a74a352f3aef
Andrey Salomatin <filipovskii.off@gmail.com>
2024-12-23 22:55:08 +0100

    Try deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://flpvsk.github.io/log-into-earth/",
   plugins: [
     preact({
       prerender: {
-        enabled: true,
+        enabled: false,
         renderTarget: "#app",
         additionalPrerenderRoutes: ["/404"],
         previewMiddlewareEnabled: true,
