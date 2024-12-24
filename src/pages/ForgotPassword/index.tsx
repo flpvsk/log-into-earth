fe85d8c0031049e0d8651363736a03c98b4efdad
flpvsk <filipovskii.off@gmail.com>
2024-12-24 09:01:49 +0100

    Test lint-staged

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -6,8 +6,11 @@ export function ForgotPassword() {
   return (
     <div class="dialog">
       <h1 class="dialog-header">Forgot password</h1>
-      <p>Password "admin" leads to a server error.</p>
-      <p>Password with less than 5 characters is invalid.</p>
+      <p>Password "admin" gives to a server error.</p>
+      <p>
+        Password with less than 5 characters gives an invalid login-password
+        combination error.
+      </p>
       <p>
         Any valid email and a password with more than 5 characters will log you
         in.
