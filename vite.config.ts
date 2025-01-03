580a5a2866d1b00512e0505535c21c4414cb8ac6
flpvsk <filipovskii.off@gmail.com>
2025-01-03 16:24:38 +0100

    Demo broken prerender

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     preact({
       prerender: {
-        enabled: false,
+        enabled: true,
         renderTarget: "#app",
         additionalPrerenderRoutes: ["/404"],
         previewMiddlewareEnabled: true,
